295b423c2f22a4ddb9e273747afafcd5afb2791d
parsasi <mail@parsa.pro>
2022-10-24 18:54:21 -0700

    `pokemonSearchSchema` is implemented

--- src/validation/pokemon.ts
+++ src/validation/pokemon.ts
@@ -1,9 +1,15 @@
-import { object } from 'yup';
+import { object, string } from "yup";
 
 // Value must have at least 2 characters
 // Value must not have numbers
 // It should a clear message when the error is present
 // Field is required
-export const pokemonSearchSchema = object({
 
+const NO_NUMBER_REGEXP = /^([^0-9]*)$/;
+
+export const pokemonSearchSchema = object({
+  searchValue: string()
+    .min(2)
+    .required("Please enter a pokemon name")
+    .matches(NO_NUMBER_REGEXP, "Pokemon name should not contain numbers"),
 });
